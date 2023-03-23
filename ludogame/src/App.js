e0697ff0b050c35f99ea31d1d8ab956e889c3bbb
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-03-24 05:22:35 +0530

    css_started

--- ludogame/src/App.js
+++ ludogame/src/App.js
@@ -1,26 +1,64 @@
 // import logo from './logo.svg';
+ import { useEffect, useRef } from 'react';
 import './App.css';
 import HomeBox from './Component/HomeBox';
+ import music from "./Media/song.mp3"
+
+  // document.getElementById("song").play();
 
 function App() {
+   const song=useRef(null);
+   const playSong=()=>{
+     song.current.play();
+     
+   }
+  //  useEffect(()=>{
+  //   playSong();
+  //  })
+
+ 
   return (
     <div className="App">
+
+
+  
       <div className='container'>
-        <div>
-        <HomeBox color={"red"}/>
-        </div>
-        <div>
-        <HomeBox color={"red"}/>
-        </div>
-        <div>
-        <HomeBox color={"red"}/>
-        </div>
-        <div>
-        <HomeBox color={"red"}/>
-        </div>
        
+        <HomeBox color={"rgb(233, 16, 16)"}/>
+      
+        <div className='path'  style={{width:"110px",height:"181px",border:"1px solid black", display:"grid",gridTemplateColumns:"repeat(3,1fr)",  boxShadow: "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px",}} >
+
+        </div>
+        
+        <HomeBox color={"rgb(10, 238, 10)"}/>
+    
+        <div className='path' style={{width:"181px",height:"110px",border:"1px solid black",gridTemplateColumns:"repeat(6,1fr)",  boxShadow: "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px",}}>
+
+        </div>
+        <div className='Home'>
+            
+       </div>
+      
+      
+        <div className='path' style={{width:"181px",height:"110px",border:"1px solid black",gridTemplateColumns:"repeat(3,1fr)",  boxShadow: "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px",}}>
+
+        </div>
+        <HomeBox color={"rgba(6, 50, 247, 0.767)"}/>
+    
+      
+      
+        <div className='path' style={{width:"110px",height:"181px",border:"1px solid black",gridTemplateColumns:"repeat(6,1fr)",  boxShadow: "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px",}}>
+
+        </div>
+        <HomeBox color={"rgb(240, 240, 16)"}/>
+      
 
       </div>
+      <audio src={music} id="song" ref={song} type="audio/mp3">
+        
+      </audio>
+      {/* <button onClick={playSong}>Play</button> */}
+     
      
     </div>
   );
--- ludogame/src/Component/HomeBox.jsx
+++ ludogame/src/Component/HomeBox.jsx
@@ -1,27 +1,47 @@
 import  "../App.css"
+import 'font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 export default function HomeBox({color}){
 
     const boxStyle={
-        width: "180px",
-        height: "180px",
-        border: "1px solid black",
+        width: "150px",
+        height: "150px",
+        // border: "1px solid black",
         backgroundColor: `${color}`,
         display: "flex",
         flexWrap: "wrap",
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        boxShadow: `${color} 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px`,
 
     }
 
     const circleStyle={
       width: "30%",
       height: "30%",
-      border: "1px solid black",
+       border: `2px solid ${color}`,
       margin: "auto",
-      borderRadius: "15px",
-      backgroundColor: `${color}`
+      //  marginTop:"2px",
+        borderRadius: "15px",
+      backgroundColor: "white",
+     
+    }
+
+    const tockStyle={
+      width: "1.5rem",
+      margin:"auto",
+       marginTop:"-1px",
+      borderRadius:"15px",
+      
+       boxShadow: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
+    
+      
+      // position:"relative"
+      fill: `${color}`,
+      // fill:"inset"
+      
     }
     
 
@@ -29,13 +49,22 @@ export default function HomeBox({color}){
       <div className="red" style={boxStyle}>
         <div >
         <div style={circleStyle}></div>
+       
         <div style={circleStyle}></div>
         <div style={circleStyle}></div>
         <div style={circleStyle}></div>
 
         </div>
+        <div style={{position:"absolute",backgroundColor:"transparent"}}>
+        <svg style={tockStyle}  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M384 192c0 87.4-117 243-168.3 307.2c-12.3 15.3-35.1 15.3-47.4 0C117 435 0 279.4 0 192C0 86 86 0 192 0S384 86 384 192z"/></svg>
         
+        <svg style={tockStyle}  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M384 192c0 87.4-117 243-168.3 307.2c-12.3 15.3-35.1 15.3-47.4 0C117 435 0 279.4 0 192C0 86 86 0 192 0S384 86 384 192z"/></svg>
+        <svg style={tockStyle}  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M384 192c0 87.4-117 243-168.3 307.2c-12.3 15.3-35.1 15.3-47.4 0C117 435 0 279.4 0 192C0 86 86 0 192 0S384 86 384 192z"/></svg>
+        <svg style={tockStyle}  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M384 192c0 87.4-117 243-168.3 307.2c-12.3 15.3-35.1 15.3-47.4 0C117 435 0 279.4 0 192C0 86 86 0 192 0S384 86 384 192z"/></svg>
 
+        </div>
+        <FontAwesomeIcon icon="fa-regular fa-location-pin" />
+       
       </div>
 
     </>
